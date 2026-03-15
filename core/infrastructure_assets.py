f2a8d26dd8b76e4a6c2349d123387c52bbb7e0af
Hadiza Nass Sani <amatuasanusi@gmail.com>
2026-03-16 00:28:46 +0100

    Create infrastructure_assets.py
    Infrastructure Asset Types

---
+++ core/infrastructure_assets.py
@@ -0,0 +1,21 @@
+class Generator:
+
+    def __init__(self, name, capacity):
+
+        self.name = name
+        self.capacity = capacity
+
+
+class Substation:
+
+    def __init__(self, name):
+
+        self.name = name
+
+
+class LoadCenter:
+
+    def __init__(self, name, demand):
+
+        self.name = name
+        self.demand = demand
